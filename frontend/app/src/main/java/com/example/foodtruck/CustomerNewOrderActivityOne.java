b1908e321372926adf2cc3535f5bac97a943236d
Vipaa <manuel.naumann@web.de>
2020-07-07 18:27:29 +0200

    some customer activitys

---
+++ frontend/app/src/main/java/com/example/foodtruck/CustomerNewOrderActivityOne.java
@@ -0,0 +1,14 @@
+package com.example.foodtruck;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CustomerNewOrderActivityOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_order_menu);
+    }
+
+}
