6d461f0d1b7492a54f54a019fa5328bfa4908d9a
DerMandelmann <67319938+DerMandelmann@users.noreply.github.com>
2020-07-07 14:46:41 +0200

    OwnerMenuActivity erstellt
    MainActivity "bereinigt"

--- frontend/app/src/main/java/com/example/helloworld/MainActivity.java
+++ frontend/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -12,25 +12,6 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_login);
     }
 
-    public void login(View v) {
-        setContentView(R.layout.activity_owner_menu);
-    }
-
-    public void registrieren(View v) {
-        setContentView(R.layout.activity_registrieren);
-    }
-
-    public void reg_abbrechen(View v) {
-        setContentView(R.layout.activity_login);
-    }
-
-    public void ownerHome(View v) {
-        setContentView(R.layout.activity_owner_menu);
-    }
-
-    public void speisekarte(View v) {
-        setContentView(R.layout.activity_owner_speisekarte);
-    }
 
 
 }
