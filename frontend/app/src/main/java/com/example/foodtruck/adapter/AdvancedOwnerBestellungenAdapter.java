74071defd3bbae6c82d06b2dc7ef218060a820b2
DerMandelmann <67319938+DerMandelmann@users.noreply.github.com>
2020-07-08 12:34:48 +0200

    Layouts geändert

---
+++ frontend/app/src/main/java/com/example/foodtruck/adapter/AdvancedOwnerBestellungenAdapter.java
@@ -0,0 +1,44 @@
+package com.example.foodtruck.adapter;
+
+        import android.content.Context;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ArrayAdapter;
+        import android.widget.TextView;
+
+        import com.example.foodtruck.R;
+        import com.example.foodtruck.model.Location;
+
+        import java.util.Objects;
+
+
+public class AdvancedOwnerBestellungenAdapter extends ArrayAdapter<Location> {
+
+    public AdvancedOwnerBestellungenAdapter(Context context, int textviewResourceId, Location[] objects){
+        super(context, textviewResourceId, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View element = convertView;
+        if (element == null) {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            element = inflater.inflate(R.layout.activity_owner_bestellungen_item, null);
+        }
+
+        TextView standort_textView = element.findViewById(R.id.standort_textView);
+        TextView ankunft_textView = element.findViewById(R.id.ankunft_textView);
+        TextView abfahrt_textView = element.findViewById(R.id.abfahrt_textView);
+
+        standort_textView.setText(getItem(position).getName());
+        String arrivalTime = Objects.requireNonNull(getItem(position)).getArrival().getHour() + ":" + Objects.requireNonNull(getItem(position)).getArrival().getMinute();
+        ankunft_textView.setText(arrivalTime);
+        String departureTime = Objects.requireNonNull(getItem(position)).getDeparture().getHour() + ":" + Objects.requireNonNull(getItem(position)).getDeparture().getMinute();
+        abfahrt_textView.setText(departureTime);
+
+        return element;
+    }
+
+}
+
