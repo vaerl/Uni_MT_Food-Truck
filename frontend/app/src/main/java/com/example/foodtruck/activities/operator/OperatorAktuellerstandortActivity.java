42fca7bef27e25172d75cb6a3186529805663c91
vaerl <47329821+vaerl@users.noreply.github.com>
2020-07-15 22:39:20 +0200

    Fix Aktueller Standort

--- frontend/app/src/main/java/com/example/foodtruck/activities/operator/OperatorAktuellerstandortActivity.java
+++ frontend/app/src/main/java/com/example/foodtruck/activities/operator/OperatorAktuellerstandortActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -25,7 +26,13 @@ public class OperatorAktuellerstandortActivity extends AppCompatActivity {
     RequestQueue queue;
     Location location;
     EditText annahmeEditText;
+    TextView annahmeAusloesen;
+    Button postAnnahme;
+    TextView annahmeMin;
     EditText schlussEditText;
+    TextView schlussAusloesen;
+    Button postSchluss;
+    TextView schlussMin;
     TextView locationStatus;
 
     @Override
@@ -33,7 +40,13 @@ public class OperatorAktuellerstandortActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_operator_aktuellerstandort);
         annahmeEditText = findViewById(R.id.reservierungsannahme_number);
+        annahmeAusloesen = findViewById(R.id.textView20);
+        postAnnahme = findViewById(R.id.reservierungsannahme_button);
+        annahmeMin = findViewById(R.id.textView21);
         schlussEditText = findViewById(R.id.reservierungsschluss_number);
+        schlussAusloesen = findViewById(R.id.textView17);
+        postSchluss = findViewById(R.id.reservierungsschluss_button);
+        schlussMin = findViewById(R.id.textView19);
         locationStatus = findViewById(R.id.location_status_textview);
         queue = Volley.newRequestQueue(this);
 
@@ -41,18 +54,33 @@ public class OperatorAktuellerstandortActivity extends AppCompatActivity {
         GsonRequest<Location, Location> request = new GsonRequest<>(Request.Method.GET, DataService.BACKEND_URL + "/operator/" + DataService.OPERATOR_ID + "/location", Location.class, DataService.getStandardHeader(), response -> {
             if (response != null) {
                 this.location = response;
-                if (this.location.getDuration() != null) {
-                    // hide textboxes
-                    annahmeEditText.setVisibility(View.INVISIBLE);
-                    schlussEditText.setVisibility(View.INVISIBLE);
-                    // show text: leaving in <minutes>
-                    locationStatus.setText("Der Foodtruck verlässt " + location.getName() + " in " + location.getDuration().toMinutes() + " Minuten.");
-                    locationStatus.setVisibility(View.VISIBLE);
-                }
+                hide();
             }
         }, error -> Log.e(TAG, "Could not post annahme!", error));
         queue.add(request);
     }
+
+    public void hide() {
+        if (!this.location.getStatus().toString().equalsIgnoreCase("open")) {
+            // hide textboxes
+            annahmeEditText.setVisibility(View.INVISIBLE);
+            annahmeAusloesen.setVisibility(View.INVISIBLE);
+            postAnnahme.setVisibility(View.INVISIBLE);
+            annahmeMin.setVisibility(View.INVISIBLE);
+            schlussEditText.setVisibility(View.INVISIBLE);
+            schlussAusloesen.setVisibility(View.INVISIBLE);
+            postSchluss.setVisibility(View.INVISIBLE);
+            schlussMin.setVisibility(View.INVISIBLE);
+            // show text: leaving in <minutes>
+            if (location.getDuration() != null) {
+                locationStatus.setText("Der Foodtruck verlässt " + location.getName() + " in " + location.getDuration().toMinutes() + " Minuten.");
+            } else {
+                locationStatus.setText("Der Foodtruck verlässt " + location.getName() + ".");
+            }
+            locationStatus.setVisibility(View.VISIBLE);
+        }
+    }
+
 
     public void annahmeAusloesen(View v) {
         String annahmeString = annahmeEditText.getText().toString();
@@ -63,9 +91,11 @@ public class OperatorAktuellerstandortActivity extends AppCompatActivity {
         Duration duration = Duration.ofMinutes(Long.parseLong(annahmeString));
         Log.d(TAG, "post location: try to post annahme");
         GsonRequest<Duration, Duration> request = new GsonRequest<>(Request.Method.POST, DataService.BACKEND_URL + "/location/" + location.getId() + "/arrive", duration, Duration.class, DataService.getStandardHeader(), response -> {
+            this.location.setStatus(Location.Status.LEAVING);
             if (response != null) {
                 location.setDuration(response);
             }
+            hide();
         }, error -> Log.e(TAG, "Could not post annahme!", error));
         queue.add(request);
     }
@@ -79,9 +109,11 @@ public class OperatorAktuellerstandortActivity extends AppCompatActivity {
         Duration duration = Duration.ofMinutes(Long.parseLong(schlussString));
         Log.d(TAG, "post location: try to post schluss");
         GsonRequest<Duration, Duration> request = new GsonRequest<>(Request.Method.POST, DataService.BACKEND_URL + "/location/" + location.getId() + "/leave", duration, Duration.class, DataService.getStandardHeader(), response -> {
+            this.location.setStatus(Location.Status.LEAVING);
             if (response != null) {
                 location.setDuration(response);
             }
+            hide();
         }, error -> Log.e(TAG, "Could not post schluss!", error));
         queue.add(request);
     }
